29895b80b4872a9e56096e9b4a871e12ff718694
PetyaYakimova <petya_yakimova@abv.bg>
2023-12-24 10:40:29 +0200

    Solution for the first task - Sum of GCP and LCM

---
+++ Softuniada-2022/SumOfGCPAndLCM/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace SumOfGCPAndLCM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            //Find the max common devider
+            int maxCommonDevider = firstNumber >= secondNumber ? secondNumber : firstNumber;
+            while (maxCommonDevider > 1)
+            {
+                if (firstNumber % maxCommonDevider == 0 && secondNumber % maxCommonDevider == 0)
+                {
+                    break;
+                }
+                maxCommonDevider--;
+            }
+
+            //Find the min multiple
+            int minMultiple = firstNumber >= secondNumber ? firstNumber : secondNumber;
+            while (minMultiple <= int.MaxValue)
+            {
+                if (minMultiple % firstNumber == 0 && minMultiple % secondNumber == 0)
+                {
+                    break;
+                }
+                minMultiple++;
+            }
+
+            Console.WriteLine(maxCommonDevider + minMultiple);
+        }
+    }
+}
