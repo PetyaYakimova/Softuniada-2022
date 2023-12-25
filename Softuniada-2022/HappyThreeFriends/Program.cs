7ea81edc8b812a26db43fdcbd47331a5fc535c07
PetyaYakimova <petya_yakimova@abv.bg>
2023-12-25 11:57:24 +0200

    Solution for the fifth task - Happy Three Friends

---
+++ Softuniada-2022/HappyThreeFriends/Program.cs
@@ -0,0 +1,77 @@
+﻿namespace HappyThreeFriends
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfOrderedWineCaskets = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfOrderedWineCaskets; i++)
+            {
+                int[] wineBottlesPrices = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+                if (IsSplittingBottlesSuccessfull(wineBottlesPrices))
+                {
+                    Console.WriteLine("Yes");
+                }
+                else
+                {
+                    Console.WriteLine("No");
+                }
+            }
+        }
+
+        public static bool IsSplittingBottlesSuccessfull(int[] wineBottles)
+        {
+            int sum = wineBottles.Sum();
+            if (sum % 3 != 0)
+            {
+                return false;
+            }
+
+            int eachPersonShare = sum / 3;
+            if (wineBottles.Any(w => w > eachPersonShare))
+            {
+                return false;
+            }
+
+            List<int> orderedWineBottleByPrice = wineBottles.OrderByDescending(w => w).ToList();
+            int successfullCombinings = 0;
+            while (orderedWineBottleByPrice.Any() && successfullCombinings < 2)
+            {
+                int currentBottle = orderedWineBottleByPrice[0];
+                orderedWineBottleByPrice.RemoveAt(0);
+                if (currentBottle == eachPersonShare)
+                {
+                    successfullCombinings++;
+                    continue;
+                }
+
+                int remainingValue = eachPersonShare - currentBottle;
+                if (orderedWineBottleByPrice.Any(w => w == remainingValue))
+                {
+                    orderedWineBottleByPrice.Remove(remainingValue);
+                    successfullCombinings++;
+                    continue;
+                }
+
+                for (int i = 1; i < remainingValue; i++)
+                {
+                    if (orderedWineBottleByPrice.Any(w => w == i) && orderedWineBottleByPrice.Any(w => w == remainingValue - i))
+                    {
+                        orderedWineBottleByPrice.Remove(i);
+                        orderedWineBottleByPrice.Remove(remainingValue - i);
+                        successfullCombinings++;
+                        break;
+                    }
+
+                    if (i == remainingValue - 1)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
