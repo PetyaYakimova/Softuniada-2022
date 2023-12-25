be79c2b4f485c02953d8c1762a6ca2b6c14aac3f
PetyaYakimova <petya_yakimova@abv.bg>
2023-12-25 10:36:51 +0200

    Solution for the third task - sorted array

---
+++ Softuniada-2022/SortedArray/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace SortedArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int arrayLength = int.Parse(Console.ReadLine());
+            int[] inputArray = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            for (int i = 0; i < arrayLength - 1; i++)
+            {
+                if ((i % 2 == 0 && inputArray[i] < inputArray[i + 1]) || (i % 2 != 0 && inputArray[i] > inputArray[i + 1]))
+                {
+                    int temp = inputArray[i];
+                    inputArray[i] = inputArray[i + 1];
+                    inputArray[i + 1] = temp;
+                }
+            }
+
+            Console.WriteLine(string.Join(' ', inputArray));
+        }
+    }
+}
